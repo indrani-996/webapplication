5e73c2d01d35595e7109fc1ea39aef97a1fd2b04
indrani <indranisethi123@Wgmail.com>
2022-11-20 11:18:50 +0000

    initial commit

---
+++ application1/src/main/java/com/Bigbasket2/App.java
@@ -0,0 +1,13 @@
+package com.Bigbasket2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
