353c53bea895d3ec4cedd9c8ce3d06a25a7e0987
indrani-996 <115713627+indrani-996@users.noreply.github.com>
2022-11-26 11:26:56 +0530

    Update App.java

--- application1/src/main/java/com/Bigbasket2/App.java
+++ application1/src/main/java/com/Bigbasket2/App.java
@@ -4,10 +4,16 @@ package com.Bigbasket2;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    
+    private final String message = "Hello World,Good Mrng To All";
+    
+    public App() {}
+    public static void main( String[] args ) {
+        System.out.println( new App().getMessage() );
     }
+    
+    private final String getMessage() {
+        return message;
+    }    
 }
